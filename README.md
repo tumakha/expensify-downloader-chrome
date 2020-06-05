96d2cb791ee3009d4b3dc211e9a13e68e62d8282
Yuriy Tumakha <tumakha@gmail.com>
2020-06-05 09:53:23 +0100

    v0.1.1

---
+++ README.md
@@ -0,0 +1,15 @@
+## Expensify Downloader - Chrome extension 
+
+Chrome extension to download expense reports selected by search on www.expensify.com
+
+### Install from sources
+
+Install Chrome extension in [Developer Mode](https://developer.chrome.com/extensions/getstarted#manifest)
+
+### Usage
+
+Go to Chrome Settings [chrome://settings](chrome://settings/), section "Advanced" => "Downloads", 
+specify Download directory and disable option "Ask where to save each file before downloading" to avoid popups for each file.
+
+Go to https://www.expensify.com/reports specify the filter to select reports then click on extension icon on top right corner.
+It will start downloading all reports selected by the filter. 
--- js/background.js
+++ js/background.js
@@ -1,11 +1,45 @@
-chrome.browserAction.setBadgeText({text: ""});
+const reportsUrl = "https://www.expensify.com/reports";
 
-document.body.style.border = "1px solid red";
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-chrome.browserAction.onClicked.addListener(function() {
-    var badge = chrome.browserAction.getBadgeText()
-    if (badge === "") {
-        badge = "ON";
+function collectLinks() {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.tabs.executeScript({code: 'collectReportLinks()'});
+}
+
+async function downloadAll(reportLinks) {
+    for (let i = 0; i < reportLinks.length; i++) {
+        chrome.tabs.create({url: reportLinks[i]}, function (tab) {
+            chrome.tabs.executeScript({file: "js/download-pdf.js"});
+        });
+
+        await delay(8000);
     }
-    chrome.browserAction.setBadgeText({text: badge});
+
+    chrome.browserAction.setBadgeText({text: ""});
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (tab.url.startsWith(reportsUrl)) {
+        collectLinks();
+    } else {
+        chrome.tabs.create({url: reportsUrl});
+    }
+});
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.reportLinks) {
+        const count = msg.reportLinks.length
+        const title = count > 0 ? 'Download Reports' : 'No Reports';
+        const message = count > 0 ? 'Downloading ' + count + ' reports...' : 'Specify filter to find reports';
+
+        chrome.notifications.create(null, {
+            type: 'basic',
+            iconUrl: 'icons/icon1024.png',
+            title: title,
+            message: message
+        });
+
+        downloadAll(msg.reportLinks);
+    }
 });
