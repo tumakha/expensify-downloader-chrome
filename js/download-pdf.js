1889a4a106f2d78e3cff922f0567878fbebf03d5
Yuriy Tumakha <Yuriy.Tumakha@iplato.com>
2021-06-27 17:02:46 +0100

    Handle new download pdf scenario

--- js/download-pdf.js
+++ js/download-pdf.js
@@ -1,15 +1,18 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function downloadPdf() {
-    waitThenClick('button.report-details-button')
-    waitThenClick('button.expensicons-download')
-
-    while (!document.querySelector('div.pdf_message a.btn')) {
-        await delay(1000)
-    }
+    await waitThenClick('button.report-details-button')
+    await waitThenClick('button.expensicons-download')
+    await waitFor('div.pdf_message a.btn')
 
     await delay(4000)
     window.close();
+}
+
+async function waitFor(selector) {
+    while (!document.querySelector(selector)) {
+        await delay(1000)
+    }
 }
 
 async function waitThenClick(selector) {
